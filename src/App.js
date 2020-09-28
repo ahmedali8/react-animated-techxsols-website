489428e8225be5363965a8c0a0cada3797d0363d
ahmedali8 <imahmedalibhatti@gmail.com>
2020-09-28 18:19:21 +0500

    created app structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { NavBar, Home, Services, AboutUs, ContactUs, Footer, NotFound } from './components';
+import { Routes, Route } from 'react-router-dom';
 
-
-function App() {
+function App () {
     return (
         <div className="app">
-            Hello World
+            <NavBar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/services" element={<Services />} />
+                <Route path="/about-us" element={<AboutUs />} />
+                <Route path="contact-us" element={<ContactUs />} />
+
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+            <Footer />
         </div>
     );
 }
