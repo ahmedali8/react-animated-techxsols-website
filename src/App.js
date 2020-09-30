f6a9efaaa3bdc288ef9237035b3a6354242fa0f8
ahmedali8 <imahmedalibhatti@gmail.com>
2020-09-30 22:09:55 +0500

    added animation fade in to app

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import './App.css';
 import { NavBar, Home, Services, AboutUs, ContactUs, Footer, NotFound } from './components';
 import { Routes, Route } from 'react-router-dom';
 
+import useWebAnimations, { fadeIn } from "@wellyshen/use-web-animations";
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
 
 
-function App() {    
+function App() {
+
+    const { ref } = useWebAnimations({
+        ...fadeIn,
+        timing: {
+            duration: 2000
+        }
+    });
+
     return (
-        <div className="app">
+        <div className="app" ref={ref}>
             <NavBar />
             <Routes>
                 <Route path="/" element={<Home />} />
