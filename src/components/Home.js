e789579202de2ffb022163e3eb404cc0bc31bbd8
ahmedali8 <imahmedalibhatti@gmail.com>
2020-09-30 18:34:19 +0500

    Home component complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import Common from './Common';
+
+import { homeSvgData } from '../data';
+
 
 const Home = () => {
     return (
-        <div>
-            Home
-        </div>
+        <>
+            <Common
+                title="Grow your business with"
+                subtitle="We are your one stop-solution in the world of digital complexion. Our dedicated team offers best output for our client's inspiration that assist them out in getting online success for their future."
+                svgData={homeSvgData}
+                linkTo="/services"
+                btnName="Services"
+            />
+        </>
     );
 };
 
