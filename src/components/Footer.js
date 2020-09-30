ea4ca06087820452bc9e12334814940314a478e6
ahmedali8 <imahmedalibhatti@gmail.com>
2020-09-30 20:06:11 +0500

    completed Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { footerData } from '../data';
+
 
 const Footer = () => {
     return (
-        <div>
-            Footer
-        </div>
+        <footer className="footer w-100 text-center mt-auto py-3">
+            <div className="container">
+                <span className="text-muted">Made by Ahmed Ali Bhatti</span>
+                <br />
+                {
+                    footerData.map(({ link, iconLink }, index) => (
+                        <a
+                            key={index}
+                            className="link"
+                            href={link}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <img src={iconLink} alt={iconLink} />
+                        </a>
+                    ))
+                }
+            </div>
+        </footer>
     );
 };
 
