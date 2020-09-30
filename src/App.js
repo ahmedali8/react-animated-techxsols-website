160f5f1bff5515be75effbc11d1c37a327c104ae
ahmedali8 <imahmedalibhatti@gmail.com>
2020-09-30 21:23:05 +0500

    fixed the layout

--- src/App.js
+++ src/App.js
@@ -7,9 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
 
 
-function App() {
-
-    
+function App() {    
     return (
         <div className="app">
             <NavBar />
