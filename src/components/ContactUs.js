d5f7f7e924098d6f5c0e7d0322af3677c83b0bc7
ahmedali8 <imahmedalibhatti@gmail.com>
2020-09-30 18:46:18 +0500

    Contact us component complete

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,9 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ContactUs = () => {
+
+    const initialState = {
+        name: '',
+        email: '',
+        message: ''
+    };
+
+    const [data, setData] = useState(initialState);
+
+    const handleChange = (e) => {
+
+        const { name, value } = e.target;
+
+        setData((preVal) => {
+            return {
+                ...preVal,
+                [name]: value
+            }
+        });
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        alert(`
+            Name: ${data.name}
+            Email: ${data.email}
+            Messaage: ${data.message}
+        `);
+
+        setData(initialState);
+    }
+
     return (
-        <div>
-            Contact Us
+        <div className="my-5">
+            <h1 className="text-center">Contact Us</h1>
+            <div className="container container_div">
+                <div className="row">
+                    <div className="col-md-6 col-10 mx-auto">
+                        <form onSubmit={onSubmit}>
+                            <div className="mb-3">
+                                <label htmlFor="name">Name</label>
+                                <input required type="text" className="form-control" name="name" value={data.name} onChange={handleChange} id="name" placeholder="Enter name here..." />
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="email">Email address</label>
+                                <input required type="email" className="form-control" name="email" value={data.email} onChange={handleChange} id="email" placeholder="Enter email here..." />
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="message">Your Message</label>
+                                <textarea required className="form-control" name="message" value={data.message} onChange={handleChange} id="message" rows="3"></textarea>
+                            </div>
+                            <div className="mb-3">
+                                <button className="btn btn-outline-primary" type="submit">Submit form</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
