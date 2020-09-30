bab941eb77f1ae6c3c814aa1d03c38d24040d745
ahmedali8 <imahmedalibhatti@gmail.com>
2020-09-30 17:52:44 +0500

    data file for images and svgs

---
+++ src/data/index.js
@@ -0,0 +1,48 @@
+export const imgData = [
+    {
+        imgsrc: 'shoe-16.jpg',
+        title: 'web development'
+    },
+    {
+        imgsrc: 'shoe-16.jpg',
+        title: 'web development'
+    },
+    {
+        imgsrc: 'shoe-16.jpg',
+        title: 'web development'
+    },
+    {
+        imgsrc: 'shoe-16.jpg',
+        title: 'web development'
+    },
+    {
+        imgsrc: 'shoe-16.jpg',
+        title: 'web development'
+    },
+    {
+        imgsrc: 'shoe-16.jpg',
+        title: 'web development'
+    }
+];
+
+export const homeSvgData = [
+    {
+        svgsrc: 'assets/svg/home_web_development.svg',
+        active: 'active'
+    },
+    {
+        svgsrc: 'assets/svg/home_creative.svg'
+    },
+    {
+        svgsrc: 'assets/svg/home_landing_page_1.svg'
+    },
+    {
+        svgsrc: 'assets/svg/home_landing_page_2.svg'
+    },
+    {
+        svgsrc: 'assets/svg/home_marketing.svg'
+    },
+    {
+        svgsrc: 'assets/svg/home_code_development.svg'
+    },
+];
